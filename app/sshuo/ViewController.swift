85b8c1558cc83629570cfa522e642ea2f66965bc
huanganqi <1079255145@qq.com>
2020-11-17 15:26:42 +0800

    使用POD

---
+++ app/sshuo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sshuo
+//
+//  Created by ume on 2020/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- sshuo/Module/Advert/Advert.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Advert.swift
-//  sshuo
-//
-//  Created by ume on 2020/11/17.
-//
-
-import UIKit
